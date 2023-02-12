61099c1dc3d04ef79e3330faa08838bab9da0bf3
Marco <marcocarrilloosuna@gmail.com>
2023-02-12 13:35:38 -0600

    :rocket: feat: list subs

---
+++ src/components/List.tsx
@@ -0,0 +1,24 @@
+interface Props {
+    subs: Array<{
+        nick: string
+        avatar: string
+        subMonths: number
+        description?: string
+    }>
+}
+
+export default function List({ subs }: Props) {
+    return (
+        <ul>
+            {
+                subs.map(sub => (
+                    <li key={sub.nick}>
+                        <img src={sub.avatar} alt={`Avatar for ${sub.nick}`} />
+                        <h4>{sub.nick} (<small>{sub.subMonths} {sub.subMonths < 2 ? 'mes' : 'meses'}</small>)</h4>
+                        <p>{sub.description?.substring(0, 100)}</p>
+                    </li>
+                ))
+            }
+        </ul>
+    )
+}
